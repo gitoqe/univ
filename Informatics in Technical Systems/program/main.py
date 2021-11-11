947818af42a37ad212f86a162deb56cb5e560e7a
Maxim <creativesun@yandex.ru>
2021-11-11 23:03:28 +0300

    add 1st part of work

--- Informatics in Technical Systems/program/main.py
+++ Informatics in Technical Systems/program/main.py
@@ -1,22 +1,51 @@
+import math
+
 def transorm_to_notation (number):
     result = ''
-    int_num_part = int(number // 1)
-    float_num_part = int(number % int_num_part)
-    print(int_num_part)
-    print(float_num_part)
-    # print(len(str(float_num_part)))
-    return result
+    if number < 0:
+        number *= -1
+        result = '-'
+    fract_part, whole_part = math.modf(number)
+    whole_part = find_whole_part(int(whole_part))
+    fract_part = find_fract_part(fract_part)
+    print('Whole part: ', whole_part)
+    if len(fract_part):
+        print('Fractional part: ', fract_part)
+    return result + str(whole_part) + '.' + str(fract_part)
 
-def transform_to_base (number):
-    base = 7
+def find_whole_part (number, base = 7):
+    '''Returns formatted number as string
+    
+    Parameters:
+    number (int): given number
+    base (int): given base
+
+    Returns:
+    result (str): formed number
+    '''
     result = ''
     while number > 0:
-        new_number = str(number % base)
-        print(new_number)
-        result = new_number + result
+        new_digit = str(number % base)
+        result = new_digit + result
         number = number // base
     return result
 
-print('Enter number:')
-user_nubmer = float(input())
-print( transorm_to_notation(user_nubmer) )
+def find_fract_part(number, base = 7, digit = 5):
+    print(number)
+    result = ''
+    while (digit):
+        number = number * base
+        fract_part, whole_part = math.modf(number)
+        number = fract_part
+        result += str(int(whole_part))
+        digit = digit - 1
+    return result
+
+def main():
+    print('Enter number:')
+    user_nubmer = float(input())
+    print('\nYour number with base 7:')
+    transformed_number = transorm_to_notation(user_nubmer)
+    print('Result: ', transformed_number)
+
+main()
